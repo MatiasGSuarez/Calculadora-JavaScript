5b9539246a55dfb4ba71874b416f15b675094273
Matias Suarez <88007836+MatiasGSuarez@users.noreply.github.com>
2021-12-15 17:52:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log ("Aplicacion Calculadora")
+
+function sumar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB= forma['operandoB'];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if (isNaN(resultado))
+        resultado = 'La operacion no tiene numeros';
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+}
